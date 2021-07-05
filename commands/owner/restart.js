2b1924c8c20ca077eac9622488b7c7f90ad9dc5e
s809 <43530948+s809@users.noreply.github.com>
2021-07-06 00:53:35 +0500

    Bump version

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,9 +1,20 @@
+/**
+ * @file Restart command.
+ */
 "use strict";
 
 import { execSync } from "child_process";
+import { Message } from "discord.js";
 
+/**
+ * Update and restart bot.
+ * 
+ * @param {Message} msg Message a command was sent from.
+ * @returns {boolean} Whether the execution was successful.
+ * @example restart(msg);
+ */
 async function restart(msg) {
-    execSync("git pull && npm i && ./mybot.sh");
+    execSync("git pull && npm ci && ./mybot.sh");
     return true;
 }
 
