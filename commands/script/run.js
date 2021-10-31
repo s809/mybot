e323ef5ad18f8932aa04f2a957073489559c30b0
s809 <43530948+s809@users.noreply.github.com>
2021-10-31 18:33:22 +0500

    Add script run arguments

--- commands/script/run.js
+++ commands/script/run.js
@@ -8,17 +8,28 @@ import sendLongText from "../../modules/messages/sendLongText.js";
  * @param {Message} msg
  * @param {string} name
  */
-async function runScript(msg, name) {
+async function runScript(msg, name, ...args) {
     if (name.match(/[/\\]/))
         return "Invalid script name.";
 
     if (!(name in data.scripts.callable))
         return "Script with this name does not exist.";
 
-    await sendLongText(msg.channel, sanitizePaths(await botEval(data.scripts.callable[name], msg)));
+    await sendLongText(msg.channel, sanitizePaths(await botEval(
+        data.scripts.callable[name].replaceAll(/\\?\$(\d+)/g, (match, value) => {
+            if (match[0] === "\\")
+                return match.slice(1);
+
+            let replacedValue = args[parseInt(value) - 1];
+            if (replacedValue === undefined)
+                throw new Error(`Value for $${parseInt(value)} is missing`);
+            return replacedValue;
+        }),
+        msg
+    )));
 }
 
 export const name = "run";
 export const minArgs = 1;
-export const maxArgs = 1;
+export const maxArgs = Infinity;
 export const func = runScript;
