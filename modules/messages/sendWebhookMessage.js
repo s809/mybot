04f24c3cc1be83e2bbc0f75da57870cad1812777
s809 <43530948+s809@users.noreply.github.com>
2021-11-06 14:33:32 +0500

    Fix pings in linked channels

--- modules/messages/sendWebhookMessage.js
+++ modules/messages/sendWebhookMessage.js
@@ -29,7 +29,9 @@ export async function sendWebhookMessage(msg, webhook) {
                         )
                     })
                 ),
-                disableMentions: "all"
+                allowedMentions: {
+                    parse: []
+                }
             });
         }
         catch (e) {
