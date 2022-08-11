48c0b29c653c9dec511067409666dec7c8d19975
s809 <43530948+s809@users.noreply.github.com>
2022-08-11 10:23:47 +0000

    Change reaction order in restart

--- commands/owner/restart.ts
+++ commands/owner/restart.ts
@@ -7,8 +7,8 @@ import { doRestart } from "../../modules/misc/restart";
 
 async function restart(msg: Message) {
     await doRestart(async () => {
-        await msg.reactions.resolve("🔄")?.users.remove();
         await msg.react("✅")
+        await msg.reactions.resolve("🔄")?.users.remove();
     });
 }
 
