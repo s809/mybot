8b4797cd20abc417d90c809d1933e11a7db328dd
s809 <43530948+s809@users.noreply.github.com>
2022-08-11 18:27:57 +0000

    Move remaining genText functions into module

--- handlers/genTextEvents.ts
+++ handlers/genTextEvents.ts
@@ -1,61 +1,27 @@
 import { client, data } from "../env";
 import { hasFlag, resolveFlaggableItem } from "../modules/data/flags";
-import { validateContext, makeGenSample } from "../modules/messages/genText";
+import { shouldGenerate, collectWordsFromMessage, generate } from "../modules/messages/genText";
 
 // Collect words from messages
 client.on("messageCreate", msg => {
-    if (!msg.guildId) return;
+    if (!msg.inGuild()) return;
     if (msg.author.bot || msg.webhookId) return;
 
     let channelData = data.guilds[msg.guildId].channels[msg.channelId];
     if (!hasFlag(channelData, "genText")) return;
 
-    let words = msg.content.split(/\s+/g).filter(word => word.length && word.length <= 30);
-    words.push("__genEnd");
-
-    for (let i = 0; i < words.length - 1; i++) {
-        let word = words[i];
-        let nextWord = words[i + 1];
-        let genCounters = channelData.genCounters!;
-        genCounters[word] ??= 0;
-
-        if (typeof genCounters[word] !== "number")
-            continue;
-
-        channelData.genData![word] ??= {};
-        let wordData = channelData.genData![word];
-
-        if (nextWord === "__genEnd" && Object.getOwnPropertyNames(wordData).includes("__genEnd"))
-            continue;
-
-        wordData[nextWord] ??= 0;
-
-        for (let w of Object.getOwnPropertyNames(wordData)) {
-            let count = wordData[w] * genCounters[word];
-            if (w === nextWord)
-                count++;
-            wordData[w] = count / (genCounters[word] + 1);
-        }
-        genCounters[word]++;
-    }
+    collectWordsFromMessage(msg, channelData);
 });
 
 // Message generation
 client.on("messageCreate", async msg => {
     if (!msg.inGuild()) return;
     if (msg.author.bot || msg.webhookId) return;
-    if (!validateContext(msg)) return;
+    if (!shouldGenerate(msg)) return;
     if (!hasFlag((await resolveFlaggableItem(msg, msg.channel.id))!.dataEntry, "genText")) return;
 
-    let genData = data.guilds[msg.guildId].channels[msg.channelId].genData!;
-    let text = "";
-    for (let i = 0; i < 10; i++) {
-        let newText = makeGenSample(genData);
-        if (newText.length > text.length)
-            text = newText;
-    }
     await msg.channel.send({
-        content: text,
+        content: generate(msg, 10),
         allowedMentions: {
             parse: ["users"]
         }
--- modules/messages/genText.ts
+++ modules/messages/genText.ts
@@ -1,20 +1,19 @@
+import assert from "assert";
 import { Message } from "discord.js";
 import { client, data } from "../../env";
 import { TextGenData } from "../data/models";
 
-export function makeGenSample(genData: Exclude<TextGenData["genData"], undefined>) {
+export function generateSingleSample(genData: Exclude<TextGenData["genData"], undefined>, maxWords = 30) {
     let words = Object.getOwnPropertyNames(genData);
     let nextWord = words[Math.floor(Math.random() * words.length)];
     let result = nextWord;
 
     let wordData = genData[nextWord];
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < maxWords; i++) {
         let chosenCumulativeProbability = Math.random();
         let nextWord: string | undefined;
 
         let words = Object.getOwnPropertyNames(wordData);
-        if (words.includes("__genEnd"))
-            words.splice(words.indexOf("__genEnd"));
         
         for (let word of words) {
             chosenCumulativeProbability -= wordData[word];
@@ -23,8 +22,11 @@ export function makeGenSample(genData: Exclude<TextGenData["genData"], undefined
                 break;
             }
         }
-        if (chosenCumulativeProbability > 0 || nextWord === "__genEnd")
-            break;
+        // Word must be chosen before reaching this point
+        assert(chosenCumulativeProbability <= 0);
+
+        if (nextWord === "__genEnd")
+            break; // Finished generating text
         
         result += " " + nextWord;
         wordData = genData[nextWord!];
@@ -33,25 +35,56 @@ export function makeGenSample(genData: Exclude<TextGenData["genData"], undefined
     return result;
 };
 
-export function generate(msg: Message<true>) {
+export function generate(msg: Message<true>, samples = 10) {
     let genData = data.guilds[msg.guildId].channels[msg.channelId].genData;
     if (!genData)
         throw new Error("No gen data found");
 
     let text = "";
-    for (let i = 0; i < 10; i++) {
-        let newText = makeGenSample(genData);
+    for (let i = 0; i < samples; i++) {
+        let newText = generateSingleSample(genData);
         if (newText.length > text.length)
             text = newText;
     }
     return text;
 }
 
-export function validateContext(msg: Message<true>) {
+export function shouldGenerate(msg: Message<true>, randomProbability = 1 / 50) {
     const isMention = msg.mentions.has(client.user!);
     const isReply = msg.reference?.messageId
         && msg.channel.messages.resolve(msg.reference.messageId)?.author.id === client.user!.id;
-    const isRandom = Math.random() < 1 / 50;
+    const isRandom = Math.random() < randomProbability;
 
     return isRandom || isReply || isMention;
 };
+
+export function collectWordsFromMessage(msg: Message<true>, channelData: TextGenData, maxWordLength = 30) {
+    let words = msg.content.split(/\s+/g).filter(word => word.length && word.length <= maxWordLength);
+    words.push("__genEnd");
+
+    for (let i = 0; i < words.length - 1; i++) {
+        let word = words[i];
+        let nextWord = words[i + 1];
+        let genCounters = channelData.genCounters!;
+        genCounters[word] ??= 0;
+
+        if (typeof genCounters[word] !== "number")
+            continue;
+
+        channelData.genData![word] ??= {};
+        let wordData = channelData.genData![word];
+
+        if (nextWord === "__genEnd" && Object.getOwnPropertyNames(wordData).includes("__genEnd"))
+            continue;
+
+        wordData[nextWord] ??= 0;
+
+        for (let w of Object.getOwnPropertyNames(wordData)) {
+            let count = wordData[w] * genCounters[word];
+            if (w === nextWord)
+                count++;
+            wordData[w] = count / (genCounters[word] + 1);
+        }
+        genCounters[word]++;
+    }
+}
