efae39251fc213b445c29705d1e694926fec1a65
s809 <43530948+s809@users.noreply.github.com>
2023-09-17 14:52:42 +0000

    Fix translations being broken

--- commands/test/translations.ts
+++ commands/test/translations.ts
@@ -2,29 +2,6 @@ import { ApplicationCommandOptionType } from "discord.js";
 import { CommandRequest } from "@s809/noisecord";
 import { defineCommand } from "@s809/noisecord";
 
-async function test(msg: CommandRequest, {
-    sendError
-}: {
-    sendError?: "raw" | "translate"
-}) {
-    let translator = msg.translator;
-    switch (sendError) {
-        case "raw":
-            return "Test raw error";
-        case "translate":
-            return "test_error";
-        default:
-            await msg.replyOrEdit(
-                "Global scope:\n"
-                + `common.strings.test: ${translator.translate("common.strings.test")}\n`
-                + `common.strings.test_missing: ${translator.translate("common.strings.test_missing")}\n`
-                + "Local scope:\n"
-                + `embeds.test: ${translator.translate("embeds.test")}\n`
-                + `embeds.test_missing: ${translator.translate("embeds.test_missing")}\n`
-            );
-    }
-}
-
 export default defineCommand({
     key: "translations",
     args: [{
@@ -34,10 +11,45 @@ export default defineCommand({
             key: "raw",
             value: "raw"
         }, {
-            key: "translate",
-            value: "translate"
+            key: "translate_auto",
+            value: "translate_auto"
+        },
+        {
+            key: "translate_manual",
+            value: "translate_manual"
         }],
         required: false
     }],
-    handler: test
+
+    translations: {
+        embeds: {
+            test: false
+        },
+        errors: {
+            test_error: false
+        }
+    },
+
+    handler: async (msg: CommandRequest, { sendError }, { embeds, errors }) => {
+        let translator = msg.translator;
+        switch (sendError) {
+            case "raw":
+                return "Test raw error";
+            case "translate_auto":
+                return errors.test_error;
+            case "translate_manual":
+                return "test_error";
+            default:
+                await msg.replyOrEdit(
+                    "Global scope:\n"
+                    + `common.strings.test: ${translator.translate("common.strings.test")}\n`
+                    + `common.strings.test_missing: ${translator.translate("common.strings.test_missing")}\n`
+                    + "Local scope (auto):\n"
+                    + `embeds.test: ${embeds.test.translate()}\n`
+                    + "Local scope (manual):\n"
+                    + `embeds.test: ${translator.translate("embeds.test")}\n`
+                    + `embeds.test_missing: ${translator.translate("embeds.test_missing")}\n`
+                );
+        }
+    }
 });
