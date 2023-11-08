38d1aa67610acbb73e50a83bd85b36a6c67aa6cb
s809 <43530948+s809@users.noreply.github.com>
2023-11-08 20:26:53 +0000

    server/clone: Fix community server handling server/clone: Fix cloning when some channels/roles are not deletable

--- commands/server/clone.ts
+++ commands/server/clone.ts
@@ -1,9 +1,8 @@
-import { ApplicationCommandOptionType, CategoryChannel, GuildChannel, GuildTextBasedChannel, NonThreadGuildBasedChannel, OverwriteType, Role, Snowflake, TextChannel, ThreadChannel, VoiceBasedChannel } from "discord.js";
+import { ApplicationCommandOptionType, CategoryChannel, GuildChannel, GuildTextBasedChannel, NonThreadGuildBasedChannel, OverwriteType, PermissionFlagsBits, Role, Snowflake, TextChannel, ThreadChannel, VoiceBasedChannel } from "discord.js";
 import { client } from "../../env";
 import { CommandRequest, defineCommand } from "@s809/noisecord";
-import { CommandDefinition } from "@s809/noisecord";
 
-async function cloneServer(msg: CommandRequest<true>, {
+async function cloneServer(req: CommandRequest<true>, {
     id: guildId,
     mode,
     cleanBeforeStarting
@@ -12,22 +11,29 @@ async function cloneServer(msg: CommandRequest<true>, {
     mode: string;
     cleanBeforeStarting: boolean;
 }) {
+    if ((mode === "both" || mode === "roles")
+        && !req.guild.members.me?.roles.cache.some(role => role.id != req.guild.roles.everyone.id && role.permissions.has(PermissionFlagsBits.Administrator))) {
+        return "Bot must have an administrator role above **@**everyone before proceeding.";
+    } else if (!req.guild.members.me?.permissions.has(PermissionFlagsBits.Administrator)) {
+        // Channels only
+        return "Bot must be administrator before proceeding.";
+    }
+
     let guild = await client.guilds.fetch(guildId);
 
     if (cleanBeforeStarting) {
         if (mode === "both" || mode === "channels") {
-            for (const channel of msg.guild.channels.cache.values())
-                await channel.delete();
+            for (const channel of req.guild.channels.cache.values())
+                await channel.delete().catch(() => {});
         }
-        
+
         if (mode === "both" || mode === "roles") {
-            for (const role of msg.guild.roles.cache.values()) {
-                if (role !== msg.guild.roles.everyone)
-                    await role.delete();
-            }
+            for (const role of req.guild.roles.cache.values())
+                await role.delete().catch(() => { });
         }
     }
 
+    // Mapping: Source -> Target
     let channels = new Map();
     let roles = new Map();
 
@@ -35,17 +41,10 @@ async function cloneServer(msg: CommandRequest<true>, {
     let role: Role;
     try {
         if (mode === "both" || mode === "roles") {
-            await msg.member!.roles.add(
-                await msg.guild.roles.create({
-                    name: "Server Owner",
-                    permissions: "Administrator"
-                })
-            );
-
             for (role of [...guild.roles.cache.values()].filter(x => !x.managed).sort((x, y) => y.position - x.position)) {
                 if (role.id === guild.roles.everyone.id) continue;
 
-                roles.set(role, await msg.guild.roles.create({
+                roles.set(role, await req.guild.roles.create({
                     name: role.name,
                     color: role.color,
                     hoist: role.hoist,
@@ -54,7 +53,8 @@ async function cloneServer(msg: CommandRequest<true>, {
                 }));
             }
 
-            roles.set(guild.roles.everyone, msg.guild.roles.everyone);
+            await req.guild.roles.everyone.setPermissions(guild.roles.everyone.permissions);
+            roles.set(guild.roles.everyone, req.guild.roles.everyone);
         }
     }
     catch (e) {
@@ -70,7 +70,7 @@ async function cloneServer(msg: CommandRequest<true>, {
             for (let channel of ([...guild.channels.cache.values()]
                 .filter(x => x instanceof CategoryChannel) as CategoryChannel[])
                 .sort((x, y) => x.position - y.position)) {
-                channels.set(channel, await msg.guild.channels.create({
+                channels.set(channel, await req.guild.channels.create({
                     name: channel.name,
                     type: channel.type,
                     permissionOverwrites: roles.size === 0
@@ -97,7 +97,7 @@ async function cloneServer(msg: CommandRequest<true>, {
                     topic: (<TextChannel>channel).topic ?? undefined,
                     nsfw: (<TextChannel>channel).nsfw,
                     bitrate: (<VoiceBasedChannel>channel).bitrate,
-                    userLimit: (<VoiceBasedChannel>channel).userLimit,
+                    userLimit: Math.min((<VoiceBasedChannel>channel).userLimit, 99),
                     rateLimitPerUser: (<TextChannel>channel).rateLimitPerUser,
                     parent: channels.get(channel.parent),
                     permissionOverwrites: roles.size === 0
@@ -111,9 +111,9 @@ async function cloneServer(msg: CommandRequest<true>, {
                                 type: OverwriteType.Role
                             }))
                 };
-                
+
                 try {
-                    await msg.guild.channels.create(options);
+                    await req.guild.channels.create(options);
                 }
                 catch (e) {
                     if (e.message === "Cannot execute action on this channel type" ||
@@ -124,7 +124,7 @@ async function cloneServer(msg: CommandRequest<true>, {
                     if (e.rawError.errors.bitrate) {
                         options.bitrate = parseInt(e.message.match(/\d+/)[0]);
                         try {
-                            await msg.guild.channels.create(options);
+                            await req.guild.channels.create(options);
                             continue;
                         } catch (e2) {
                             e = e2;
@@ -141,12 +141,23 @@ async function cloneServer(msg: CommandRequest<true>, {
     }
 
     if (didSkipChannels) {
-        const newTextChannel = msg.guild.channels.cache.find(x => x.isTextBased()) as GuildTextBasedChannel;
+        const newTextChannel = req.guild.channels.cache.find(x => x.isTextBased()) as GuildTextBasedChannel;
 
         await (cleanBeforeStarting
             ? newTextChannel?.send.bind(newTextChannel)
-            : msg.channel.send.bind(msg.channel)
+            : req.channel.send.bind(req.channel)
         )?.("Some channels were skipped as this server is not community-enabled.");
+    }
+
+    if (guild.systemChannelId)
+        await req.guild.setSystemChannel(channels.get(guild.systemChannel));
+
+    if (guild.safetyAlertsChannelId)
+        await req.guild.setSafetyAlertsChannel(channels.get(guild.safetyAlertsChannel));
+
+    if (guild.features.includes("COMMUNITY") && req.guild.features.includes("COMMUNITY")) {
+        await req.guild.setRulesChannel(channels.get(guild.rulesChannel));
+        await req.guild.setPublicUpdatesChannel(channels.get(guild.publicUpdatesChannel));
     }
 }
 
